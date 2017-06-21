a8b7097057133cc612ffa04a3adc5d106443260c
Hadrien Kohl <hadrien.kohl@gmail.com>
2017-06-21 13:55:35 +0200

    Support for application/ssb.dataset.structure+json;version=2

--- java-vtl-tools/src/test/java/no/ssb/vtl/tools/sandbox/connector/spring/converters/DataStructureHttpConverterTest.java
+++ java-vtl-tools/src/test/java/no/ssb/vtl/tools/sandbox/connector/spring/converters/DataStructureHttpConverterTest.java
@@ -10,6 +10,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.http.HttpInputMessage;
 
+import java.time.Instant;
 import java.util.function.BiFunction;
 
 import static no.ssb.vtl.tools.sandbox.connector.spring.converters.DataStructureHttpConverter.MEDIA_TYPE;
@@ -34,7 +35,34 @@ public class DataStructureHttpConverterTest {
         HttpInputMessage message = loadFile("ssb.dataset.structure+json;version=2" + ".json");
 
         DataStructure result = converter.read(DataStructure.class, message);
-        System.out.println(result);
+
+        softly.assertThat(result)
+                .as("name of the variables")
+                .containsKeys(
+                        "Country", "SentOn", "Measure", "Type", "Valid"
+                );
+
+        softly.assertThat(result.values())
+                .extracting(Component::getType)
+                .as("types of the variables")
+                .containsExactly(
+                        (Class) String.class,
+                        (Class) Instant.class,
+                        (Class) Double.class,
+                        (Class) Long.class,
+                        (Class) Boolean.class
+                );
+
+        softly.assertThat(result.values())
+                .extracting(Component::getRole)
+                .as("types of the variables")
+                .containsExactly(
+                        Component.Role.IDENTIFIER,
+                        Component.Role.ATTRIBUTE,
+                        Component.Role.MEASURE,
+                        Component.Role.ATTRIBUTE,
+                        Component.Role.ATTRIBUTE
+                );
     }
 
     @Test
